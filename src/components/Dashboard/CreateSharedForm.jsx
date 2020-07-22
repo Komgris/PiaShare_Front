14dbb75b4ff03d4fd8be9fcec7a9ce9779a7a5fc
devspsec <devs.psec@gmail.com>
2020-07-22 11:44:35 +0700

    form validation

--- src/components/Dashboard/CreateSharedForm.jsx
+++ src/components/Dashboard/CreateSharedForm.jsx
@@ -1,54 +1,55 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import { useForm } from "react-hook-form";
 
 export default function CreateSharedForm() {
+
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
+
     return (
         <div>
             <div className="top-panel-create">สร้างห้องแชร์</div>
-        <div className="create-form-panel">
-                <form >
+            <div className="create-form-panel">
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="container">
                         <div className="row">
-                                <div className="col-sm-12 col-md-6"><label className="label-register-container">ชื่อห้อง :</label></div>
-                                <div className="col-sm-12 col-md-6"><label className="label-register-container">เงินต้น :</label></div>
+                            <div className="col-sm-12 col-md-6"><label className="label-register-container">ชื่อห้อง :</label></div>
+                            <div className="col-sm-12 col-md-6"><label className="label-register-container">เงินต้น :</label></div>
 
-                                <div className="col-sm-12 col-md-6">
-                                    <input name="username" type="text" className="input-register-container"  ></input>
-                                </div>
-                                <div className="col-sm-12 col-md-6">
-                                    <input name="username" type="number" className="input-register-container"  ></input>
-                                </div>
-
-                                <div className="col-sm-6"><label className="label-register-container">จำนวนสมาชิก :</label></div>
-                                <div className="col-sm-6"><label className="label-register-container">รอบการเปีย :</label></div>
-                                <div className="col-sm-6">
-                                    <input name="username" type="number" className="input-register-container"  ></input>
-                                </div>
-                                <div className="col-sm-6">
-                                    <input name="username" type="text" className="input-register-container"  ></input>
-                                </div>
-       
-                                <div className="col-sm-6"><label className="label-register-container">เริ่ม :</label></div>
-                                <div className="col-sm-6"><label className="label-register-container">สิ้นสุด :</label></div>
-                                <div className="col-sm-6">
-                                    <input name="username" type="text" className="input-register-container"  ></input>
-                                </div>
-                                <div className="col-sm-6">
-                                    <input name="username" type="text" className="input-register-container"  ></input>
-                                </div>
+                            <div className="col-sm-12 col-md-6">
+                                <input name="roomName" type="text" className="input-register-container" ref={register({ required: true })}></input>
                             </div>
+                            <div className="col-sm-12 col-md-6">
+                                <input name="budget" type="number" className="input-register-container" ref={register({ required: true })}></input>
+                            </div>
+
+                            <div className="col-sm-6"><label className="label-register-container">จำนวนสมาชิก :</label></div>
+                            <div className="col-sm-6"><label className="label-register-container">รอบการเปีย :</label></div>
+                            <div className="col-sm-6">
+                                <input name="memberAmount" type="number" className="input-register-container" ref={register({ required: true })}></input>
+                            </div>
+                            <div className="col-sm-6">
+                                <input name="period" type="text" className="input-register-container" ref={register({ required: true })}></input>
+                            </div>
+
+                            <div className="col-sm-6"><label className="label-register-container">เริ่ม :</label></div>
+                            <div className="col-sm-6"><label className="label-register-container">สิ้นสุด :</label></div>
+                            <div className="col-sm-6">
+                                <input name="from" type="date" className="input-register-container" ref={register({ required: true })}></input>
+                            </div>
+                            <div className="col-sm-6">
+                                <input name="to" type="date" className="input-register-container" ref={register({ required: true })}></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="btn-login-footer">
+                        <button className="btn-footer btn-confirm-login-footer"> confrim</button>
+                        <button className="btn-footer btn-cancel-login-footer"> cancel</button>
                     </div>
                 </form>
-                <div className="btn-login-footer">
-                <button className="btn-footer btn-confirm-login-footer"> confrim</button>
-                <button className="btn-footer btn-cancel-login-footer"> cancel</button>
-                </div>
-        </div>
+            </div>
         </div>
     )
 }
